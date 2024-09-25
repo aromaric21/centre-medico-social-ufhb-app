ef019689a3617593ebefa46ad94b143d1db7f3e5
aromaric <aromaricadjei213@gmail.com>
2024-09-25 08:44:22 +0000

    Message de commit pour la modification de type email de table user

--- database/migrations/0001_01_01_000001_create_users_table.php
+++ database/migrations/0001_01_01_000001_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('nom');
             $table->string('prenom');
             $table->string('contact')->nullable();
-            $table->text('email')->unique();
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('etatCompte')->nullable();
